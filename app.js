6f71386e07b6b2ceed9cea8845c8598bb2aaceee
wouter <wouter@wercker.com>
2012-05-03 17:03:03 +0200

    'use strict';

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+    'use strict';
     response.send('Hello World!');
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
+    'use strict';
     console.log("Listening on " + port);
 });
