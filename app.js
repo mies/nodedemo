1b8b001d058b4bd5c5347ece6da60a685eb9116d
wouterm <hebrox@gmail.com>
2012-05-22 17:24:05 +0200

    green

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     'use strict';
     response.send('Hello World Domination');
-})
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
     'use strict';
     console.log("Listening on " + port);
-})
+});
