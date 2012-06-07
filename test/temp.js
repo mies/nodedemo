4eca69f510652d8be294dd9f2ca03f0beb154e3f
wouterm <hebrox@gmail.com>
2012-06-07 17:42:43 +0200

    add mysql

--- test/temp.js
+++ test/temp.js
@@ -1,9 +1,28 @@
+var mysql = require('mysql');
 /**
  * @param {Object} test node-unit test object.
  */
 exports.testSomething2 = function(test) {
     'use strict';
     test.expect(1);
+
+
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'me',
+    password : 'secret'
+  });
+
+  connection.connect();
+
+  connection.query('SELECT 1', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log('Query result: ', rows);
+  });
+
+  connection.end();
+
     test.ok(1, 'this assertion should pass');
     test.done();
 };
