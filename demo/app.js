dd72d80593effed934e5706ac7b0e53890ba4f84
wouterm <hebrox@gmail.com>
2012-06-11 11:56:32 +0200

    get to green

--- demo/app.js
+++ demo/app.js
@@ -1,6 +1,7 @@
 require('http').createServer(function (request, response) {
+  'use strict';
   response.writeHead(200, {"Content-Type": "text/plain"});
-  output = "Hello World!\n";
+  var output = "Hello World!\n";
   for (k in request.headers) {
     output += k + '=' + request.headers[k] + '\n';
   }
