ef937070e247a6ae01c984a27717eb958ccec468
wouterm <hebrox@gmail.com>
2012-06-24 20:48:41 -0700

    green

--- test/temp.js
+++ test/temp.js
@@ -45,7 +45,7 @@ exports.testSomething2 = function(test) {
  */
 exports.testSomethingEls = function(test) {
   'use strict';
-  test.expect(2);
+  test.expect(1);
   test.ok(1, 'this assertion should FAIL');
   test.done();
 };
