421b509c8de144137ae7d2744ec684dcdb992b34
wouterm <hebrox@gmail.com>
2012-06-27 13:18:15 -0700

    dotcloud changes

--- demo/app.js
+++ demo/app.js
@@ -1,6 +1,7 @@
 require('http').createServer(function (request, response) {
   'use strict';
   response.writeHead(200, {"Content-Type": "text/plain"});
+  var k;
   var output = "Hello World!\n";
   for (k in request.headers) {
     output += k + '=' + request.headers[k] + '\n';
