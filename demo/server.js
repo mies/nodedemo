cfef7ca62cd8f8a8c169fb7d6cadf8c087d9c3cf
wouterm <hebrox@gmail.com>
2012-06-27 14:06:34 -0700

    domination!

--- demo/server.js
+++ demo/server.js
@@ -2,7 +2,7 @@ require('http').createServer(function (request, response) {
   'use strict';
   response.writeHead(200, {"Content-Type": "text/plain"});
   var k;
-  var output = "Hello World!\n";
+  var output = "Hello World`Domination!\n";
   for (k in request.headers) {
     output += k + '=' + request.headers[k] + '\n';
   }
