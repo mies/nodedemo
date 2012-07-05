a8d277591f8dd70bad25049d757c30a11d03459e
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-07-05 11:55:20 -0700

    jshint

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.listen(port, function() {
 var http = require('http');
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
+  'use strict';
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World Domination\n');
 }).listen(port);
