aeb3eff470d68a1a4d264a6bfba1d9de3c570f0e
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-07-18 10:45:08 -0700

    fixed some bugs

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   'use strict';
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, Jerome!')
+  res.end('Hello, World!');
 }).listen(port);
