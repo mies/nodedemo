cbf700f088ae6ad6613e5dc9bcbe1b7d8c688d68
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-07-23 15:25:12 -0700

    making mistakes

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-/*
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-    'use strict';
-    response.send('Hello World Domination!!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    'use strict';
-    console.log("Listening on " + port);
-});
-*/
-
 var http = require('http');
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   'use strict';
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World');
+  res.end('Hello, Diane!')
 }).listen(port);
