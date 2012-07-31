36a559f87540f21d50f4f160e0db89176417d755
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-07-31 15:07:37 -0700

    mistakes!

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   'use strict';
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World');
+  res.end('Hello, Tim')
 }).listen(port);
