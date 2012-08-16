ab8e909fd82e1c606d86b715cbe5857c98f0a09a
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-08-16 15:19:16 +0200

    removed headers in demo app

--- demo/server.js
+++ demo/server.js
@@ -2,9 +2,6 @@ require('http').createServer(function (request, response) {
   'use strict';
   response.writeHead(200, {"Content-Type": "text/plain"});
   var k;
-  var output = "Hello World`Domination!\n";
-  for (k in request.headers) {
-    output += k + '=' + request.headers[k] + '\n';
-  }
+  var output = "Hello World Domination!\n";
   response.end(output);
 }).listen(8080);
